023bf24efac685c7a8064d583cc171e6ef4d31e0
Lorepelhu98 <lorenzopelhuri@libero.it>
2024-02-07 17:18:25 +0100

    terzo commit s2 js

---
+++ m1/s3/g3/index.js
@@ -0,0 +1,13 @@
+
+let input = document.createElement('input');
+let button = document.createElement('button');
+let ul = document.createElement('ul');
+
+button.textContent = 'Aggiungi Task';
+
+document.body.appendChild(input);
+document.body.appendChild(button);
+document.body.appendChild(ul);
+
+
+
