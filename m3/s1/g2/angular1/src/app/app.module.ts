d7a0bb8c97c724885ae7af4f843bca898c0ad203
Lorepelhu98 <lorenzopelhuri@libero.it>
2024-03-19 14:39:32 +0100

    commit g2 s1 m3

---
+++ m3/s1/g2/angular1/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components1/main/main.component';
+import { FooterComponent } from './component2/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
